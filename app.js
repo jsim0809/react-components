5c8ffe7204d3f7a726197b3806f8589c3122a44e
jsim0809 <jeremy_sim@hotmail.com>
2020-02-18 18:25:34 -0800

    Started

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-// TODO
+
+var GroceryListItem = (props) => {
+  return <li>{props.groceryItems[0]}</li>;
+}
+
+var GroceryList = (props) => (
+  <ul>
+    <GroceryListItem groceryItems={props.groceryItems} />
+
+  </ul>
+);
+
+ReactDOM.render(<GroceryList groceryItems={['Cucumber', 'Kale']} />, document.getElementById('app'));s
